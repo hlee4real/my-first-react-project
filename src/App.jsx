3bb603a7f00ed0d119d4f6a97d4730684a7ebec4
Lê Huy Hoàng <lehuyhoang@apples-MacBook-Pro.local>
2023-02-16 14:47:49 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import MainHeader from "./components/MainHeader";
+import PostsList from "./components/PostsList";
+import { useState } from "react";
+function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+  function hideModalHandler() {
+    setModalIsVisible(false)
+  }
+  function showModalHandler() {
+    setModalIsVisible(true)
+  } 
+
+  return (
+    <>
+      <MainHeader onCreatePost={showModalHandler}/>
+      <main>
+        <PostsList isPosing={modalIsVisible} onStopPosting={hideModalHandler}/>
+      </main>
+    </>
+  )
+    ;
+}
+
+export default App;
